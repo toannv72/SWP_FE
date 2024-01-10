607701c990c2ce18b6333edbbc1be35a772bc24e
toannv72 <64313624+toannv72@users.noreply.github.com>
2024-01-10 18:26:13 +0700

    upda like

--- src/Page/Authenticator/Home/Home.jsx
+++ src/Page/Authenticator/Home/Home.jsx
@@ -1,6 +1,5 @@
 
 import ComHeader from "../../Components/ComHeader/ComHeader";
-import images from "../../../img";
 import { useEffect, useState } from "react";
 import { getData, postData } from "../../../api/api";
 import InfiniteScroll from 'react-infinite-scroll-component';
@@ -10,13 +9,19 @@ import {
     CommentOutlined
 } from '@ant-design/icons';
 import ComPost from "../../Components/ComPost/ComPost";
+import { Image } from "antd";
+import { useNavigate } from "react-router-dom";
+import { useStorage } from "../../../hooks/useLocalStorage";
 export default function Home() {
     const [products, setProducts] = useState([]);
     const [hasMore, setHasMore] = useState(true);
     const [page, setPage] = useState(1);
     const [likedProducts, setLikedProducts] = useState([]);
     const [allUser, setAllUser] = useState([]);
-
+    const navigate = useNavigate();
+    const [token, setToken] = useStorage("user", {});
+    const [likedProductIds, setLikedProductIds] = useState([])
+    console.log(likedProductIds);
     const fetchData = async (pageNumber) => {
         try {
             const response = await getData(`/artwork?page=${pageNumber}&limit=10`);
@@ -28,22 +33,28 @@ export default function Home() {
     };
     const fetchMoreProducts = async () => {
         const newProducts = await fetchData(page + 1);
-
         if (newProducts.length === 0) {
             setHasMore(false); // No more data to load
         } else {
             setProducts([...products, ...newProducts]);
+            const userLikesArray = newProducts.map(product => product.likes.some(like => like.user === token?._doc?._id));
+
+            const likesCountArray = newProducts.map(product => product.likes.length);
+            setLikedProducts([...likedProducts, ...userLikesArray])
+            setLikedProductIds([...likedProductIds, ...likesCountArray])
             setPage(page + 1);
         }
     };
-
 
     useEffect(() => {
         const loadInitialData = async () => {
             const initialProducts = await fetchData(page);
             setProducts(initialProducts);
+            const userLikesArray = initialProducts.map(product => product.likes.some(like => like.user === token?._doc?._id));
+            const likesCountArray = initialProducts.map(product => product.likes.length);
+            setLikedProductIds(likesCountArray)
+            setLikedProducts(userLikesArray)
         };
-
         loadInitialData();
     }, []); // Run only once on component mount
 
@@ -70,19 +81,37 @@ export default function Home() {
     }, []);
 
     // useEffect để thiết lập mảng likedProducts có độ dài bằng độ dài của mảng products và mỗi phần tử có giá trị ban đầu là false
-    useEffect(() => {
-        setLikedProducts(new Array(products.length).fill(true));
-    }, [products]);
-    console.log(likedProducts);
+
+
     const handleLike = (index, id_artwork, id_user) => {
+
         const updatedLikedProducts = [...likedProducts];
         updatedLikedProducts[index] = !updatedLikedProducts[index];
         setLikedProducts(updatedLikedProducts);
 
+        const updatedLikedProductIds = [...likedProductIds];
+        updatedLikedProductIds[index] = updatedLikedProductIds[index] + 1;
+        setLikedProductIds(updatedLikedProductIds);
         postData(`/artwork/likeArtwork/${id_artwork}/${id_user}`, {})
             .then((e) => {
-                console.log(e);
+            })
+            .catch(err => {
+                console.log(err);
+            });
 
+    };
+    
+    const handleUnLike = (index, id_artwork, id_user) => {
+
+        const updatedLikedProducts = [...likedProducts];
+        updatedLikedProducts[index] = !updatedLikedProducts[index];
+        setLikedProducts(updatedLikedProducts);
+
+        const updatedLikedProductIds = [...likedProductIds];
+        updatedLikedProductIds[index] = updatedLikedProductIds[index] - 1;
+        setLikedProductIds(updatedLikedProductIds);
+        postData(`/artwork/unlikeArtwork/${id_artwork}/${id_user}`, {})
+            .then((e) => {
             })
             .catch(err => {
                 console.log(err);
@@ -102,37 +131,45 @@ export default function Home() {
             >
                 <div className="grid gap-4 justify-center ">
                     {products.map((artwork, index) => (
-
                         <div key={index} className=" w-screen bg-[#f3f9f140] sm:w-[600px] lg:w-[900px] xl:w-[1000px] xl:gap-x-8 shadow-md rounded-lg border-solid border-2 border-[#89898936] ">
                             <div className="px-2 py-1 flex items-center gap-2">
                                 <img className="inline-block h-10 w-10 object-cover rounded-full ring-2 ring-white" src={getUserById(allUser, artwork.user)?.avatar} alt="" /> <p className="text-2xl">{getUserById(allUser, artwork.user)?.name}</p>
                                 {/* <button  className="text-[#5a86ff]">Theo dõi</button> */}
-                            </div>
 
+                            </div>
                             <pre style={{ whiteSpace: "pre-wrap", wordWrap: "break-word", padding: '6px' }}>
                                 {artwork.content}
                             </pre>
-                            <ComLink key={artwork._id} to={`/artwork/${artwork._id}`} className="w-screen sm:w-[600px] lg:w-[900px] xl:w-[1000px] xl:gap-x-8 shadow-md  border-solid  border-white ">
-                                <div className="relative overflow-hidden bg-gray-200 xl:aspect-h-8 xl:aspect-w-7 ">
-                                    <img
-                                        src={artwork.image}
-                                        alt={artwork.imageAlt}
-                                        className="w-full h-full object-cover object-center lg:h-full lg:w-full "
-                                    />
-                                </div>
-                            </ComLink>
+                            <div key={artwork._id} className="w-auto  xl:gap-x-8 shadow-md  border-solid  border-white ">
+                                <div className="relative overflow-hidden bg-gray-200 xl:aspect-h-8 xl:aspect-w-7 flex justify-center ">
 
+                                    <Image.PreviewGroup
+                                        items={artwork.image}
+                                    >
+                                        <Image
+                                            maskClassName="w-full h-full object-cover object-center lg:h-full lg:w-full "
+
+                                            src={artwork.image}
+                                            alt={artwork.imageAlt}
+                                        />
+                                    </Image.PreviewGroup>
+                                </div>
+                            </div>
+                            <div className="flex justify-around mb-1 p-1 gap-10 mt-2 ">
+                                <p>{likedProductIds[index]} {artwork?.likes ? "Like" : ''}</p>
+                                <p>{artwork?.comments.length} {artwork?.comments ? "comment" : ''}</p>
+                            </div>
                             <div className="flex justify-center">
                                 <div className=" w-11/12 h-[1px] bg-[#999998] my-2">
                                 </div>
                             </div>
-
                             <div className="flex justify-around mb-1 p-1 gap-2" >
-                                <button className={`flex gap-2 w-1/2  items-center  h-8  justify-center rounded-lg hover:bg-[#f1f0f0] ${likedProducts[index] ? 'text-[#08c]' : ''}`} onClick={() => handleLike(index, artwork._id, artwork.user)}>
+                                <button className={`flex gap-2 w-1/2  items-center  h-8  justify-center rounded-lg hover:bg-[#f1f0f0] ${likedProducts[index] ? 'text-[#08c]' : ''}`}
+                                    onClick={() => { !likedProducts[index] ? handleLike(index, artwork._id, token?._doc?._id) : handleUnLike(index, artwork._id, token?._doc?._id) }} >
                                     {likedProducts[index] ? <LikeOutlined style={{ fontSize: '20px' }} /> : <LikeOutlined style={{ fontSize: '20px', }} />}
                                     <p style={likedProducts[index] ? { color: '#08c' } : {}}>{likedProducts[index] ? 'Đã thích' : 'Thích'}</p>
                                 </button>
-                                <button className="flex gap-2 w-1/2 items-center h-8  justify-center rounded-lg hover:bg-[#f1f0f0]"><CommentOutlined style={{ fontSize: '20px', }} />Bình luận</button>
+                                <button onClick={() => navigate(`/artwork/${artwork._id}`)} className="flex gap-2 w-1/2 items-center h-8  justify-center rounded-lg hover:bg-[#f1f0f0]"><CommentOutlined style={{ fontSize: '20px', }} />Bình luận</button>
                             </div>
                         </div>
                     ))}
